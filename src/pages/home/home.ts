d984fb087a02cf2c8cf8faf717c998b3338a9fae
Hygor Zorak <hygorzorak@gmail.com>
2018-01-16 18:15:53 -0200

    criação da home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from "@angular/core";
 import { NavController } from "ionic-angular";
 import { IonicPage } from "ionic-angular";
+import { ProfilePage } from "../profile/profile";
+import { ScannerPage } from "../scanner/scanner";
+import { ListPage } from "../list/list";
 
 @IonicPage()
 @Component({
@@ -9,4 +12,16 @@ import { IonicPage } from "ionic-angular";
 })
 export class HomePage {
   constructor(public navCtrl: NavController) {}
+
+  goToProfile() {
+    this.navCtrl.push(ProfilePage);
+  }
+
+  goToScanner() {
+    this.navCtrl.push(ScannerPage);
+  }
+
+  goToReport() {
+    this.navCtrl.push(ListPage);
+  }
 }
