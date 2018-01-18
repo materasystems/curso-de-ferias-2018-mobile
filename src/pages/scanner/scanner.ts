f5c2184516413185fefe03334fb67be955a6303f
Hygor Zorak <hygorzorak@gmail.com>
2018-01-18 10:45:57 -0200

    criação da página para uso do serviço

--- src/pages/scanner/scanner.ts
+++ src/pages/scanner/scanner.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
-import { IonicPage } from "ionic-angular";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
+import { NavController, AlertController, IonicPage } from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -8,5 +8,50 @@ import { IonicPage } from "ionic-angular";
   templateUrl: "scanner.html"
 })
 export class ScannerPage {
-  constructor(public navCtrl: NavController) {}
+  public barcodeData;
+
+  constructor(
+    public navCtrl: NavController,
+    public barcodeScanner: BarcodeScanner,
+    public alertCtrl: AlertController
+  ) {}
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad BarcodescannerPage");
+  }
+
+  scan() {
+    const options = {
+      preferFrontCamera: false,
+      showFlipCameraButton: true,
+      showTorchButton: true,
+      torchOn: false,
+      prompt: "Place a barcode inside the scan area", // Android
+      resultDisplayDuration: 500,
+      formats: "QR_CODE,PDF_417",
+      orientation: "portrait",
+      disableAnimations: true, // iOS
+      disableSuccessBeep: false // iOS
+    };
+
+    this.barcodeScanner
+      .scan(options)
+      .then(data => {
+        this.barcodeData = data;
+        const alert = this.alertCtrl.create({
+          title: "Scan Results",
+          subTitle: data.text,
+          buttons: ["OK"]
+        });
+        alert.present();
+      })
+      .catch(err => {
+        const alert = this.alertCtrl.create({
+          title: "Attention!",
+          subTitle: err,
+          buttons: ["Close"]
+        });
+        alert.present();
+      });
+  }
 }
