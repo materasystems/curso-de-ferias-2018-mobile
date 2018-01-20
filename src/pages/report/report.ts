ae17ff686b65966eb5c187ae5b4e3b52584f0560
Flavia Domingues <flavia.domingues@matera.com>
2018-01-20 19:35:07 -0200

    Ajustando página do relatório

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -16,34 +16,28 @@ export class ReportPage {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get("item");
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = [
-      "flask",
-      "wifi",
-      "beer",
-      "football",
-      "basketball",
-      "paper-plane",
-      "american-football",
-      "boat",
-      "bluetooth",
-      "build"
-    ];
-
     this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: "Item " + i,
-        note: "This is item #" + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.items.push({
+      title: "Angular",
+      note: "ver mais",
+      icon: "logo-angular"
+    },
+      {
+        title: "Ionic",
+        note: "ver mais",
+        icon: "ionic"
+      },
+      {
+        title: "Java",
+        note: "ver mais",
+        icon: "cafe"
+      }
+    );
   }
 
   itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ReportPage, {
-      item: item
-    });
+    if (item.title == "Java") {
+      this.navCtrl.push(ReportPage, { item: item });
+    }
   }
 }
