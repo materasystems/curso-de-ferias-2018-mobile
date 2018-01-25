632501509be371ab155f4ad56da7127bf9c308d7
Hygor Zorak <hygorzorak@gmail.com>
2018-01-25 13:48:37 -0200

    habilitando o patch

--- src/providers/user-service/user-service.ts
+++ src/providers/user-service/user-service.ts
@@ -1,20 +1,17 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Storage } from "@ionic/storage";
 
 @Injectable()
 export class UserServiceProvider {
   currentUser: Object;
 
-  constructor(public http: HttpClient, private storage: Storage) {}
+  constructor(public http: HttpClient) {}
 
   changePicture(id, img) {
     let uri = `https://curso-ferias.herokuapp.com/usuario/${id}`;
     let body = {
       urlFoto: img
     };
-    console.log(uri);
-    console.log(body);
-    // this.http.patch(uri, body);
+    this.http.patch(uri, body);
   }
 }
