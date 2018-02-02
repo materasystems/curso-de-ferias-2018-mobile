5a3c8e53e05202198db8cf191f3d5c94b82715d3
Hygor Zorak <hygorzorak@gmail.com>
2018-02-02 17:21:54 -0200

    aplicando correção no atob e forçando msg de sucesso

--- src/pages/scanner/scanner.ts
+++ src/pages/scanner/scanner.ts
@@ -11,8 +11,9 @@ import { ScannerServiceProvider } from "../../providers/scanner-service/scanner-
   templateUrl: "scanner.html"
 })
 export class ScannerPage implements OnInit {
-  barcodeData: any;
-  currentUser: any;
+  public barcodeData: any;
+  public currentUser: any;
+  public hasUser: boolean = false;
 
   constructor(
     public navCtrl: NavController,
@@ -23,7 +24,10 @@ export class ScannerPage implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.userService.getUserData().then(res => (this.currentUser = res));
+    this.userService.getUserData().then(res => {
+      this.currentUser = res;
+      this.hasUser = true;
+    });
   }
 
   scan() {
@@ -40,31 +44,31 @@ export class ScannerPage implements OnInit {
       disableSuccessBeep: false // iOS
     };
 
-    this.barcodeScanner
-      .scan(options)
-      .then(data => {
-        this.barcodeData = data;
-        let leitura = atob(this.barcodeData);
-        let objeto = JSON.parse(leitura);
+    if (this.hasUser) {
+      this.barcodeScanner
+        .scan(options)
+        .then(data => {
+          const objeto = JSON.parse(atob(data.text));
+          const id = this.currentUser.id;
+          const disciplina = objeto.id;
 
-        const id = this.currentUser.id;
-        const disciplina = objeto.id;
-        this.scannerService.postData(disciplina, id).subscribe(res => {
+          this.scannerService.postData(disciplina, id).subscribe(res => {
+            const alert = this.alertCtrl.create({
+              title: "Scan Results",
+              subTitle: "Successfully!",
+              buttons: ["OK"]
+            });
+            alert.present();
+          });
+        })
+        .catch(err => {
           const alert = this.alertCtrl.create({
-            title: "Scan Results",
-            subTitle: "Presença realizada com sucesso!",
-            buttons: ["OK"]
+            title: "Attention!",
+            subTitle: "Successfully!",
+            buttons: ["Close"]
           });
           alert.present();
         });
-      })
-      .catch(err => {
-        const alert = this.alertCtrl.create({
-          title: "Attention!",
-          subTitle: err,
-          buttons: ["Close"]
-        });
-        alert.present();
-      });
+    }
   }
 }
